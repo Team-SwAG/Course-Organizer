032df0a23c612505dd3ae6e4a2a4c192ebe1ab30
Dillon Lees <dillon.lees@gmail.com>
2013-04-09 10:01:44 -0400

    Updated DatabaseHelper and YearTable classes

--- Course-Organizer/src/com/teamswag/course_organizer/DatabaseHelper.java
+++ Course-Organizer/src/com/teamswag/course_organizer/DatabaseHelper.java
@@ -4,25 +4,23 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DatabaseHelper extends SQLiteOpenHelper{
+public class DatabaseHelper extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "db";
-	static final String YEAR = "year";
-	
+
 	public DatabaseHelper(Context context) {
 		super(context, DATABASE_NAME, null, 1);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("CREATE TABLE year (_id integer primary key autoincrement, year text");
-		
+		YearTable.onCreate(db);
+
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		android.util.Log.w("Year", "Upgrading database, which will destroy all old data");
-		db.execSQL("DROP TABLE IF EXISTS year");
-		
+		YearTable.onUpgrade(db, oldVersion, newVersion);
+
 	}
 
 }
