ef3ad6f12832a8ab37fb6966c38ff0f44cc2bf49
Dillon Lees <dillon.lees@gmail.com>
2013-04-09 00:06:28 -0400

    Did some database related stuff... it isn't right but it'll do for now.

---
+++ Course-Organizer/src/com/teamswag/course_organizer/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package com.teamswag.course_organizer;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+	
+	public DatabaseHelper(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	static final String YEAR = "year";
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Course-Organizer/src/com/teamswag/course_organizer/YearActivity.java
+++ Course-Organizer/src/com/teamswag/course_organizer/YearActivity.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -13,6 +16,8 @@ import android.widget.EditText;
 public class YearActivity extends ListActivity {
 	
 	ArrayList<String> yearList = new ArrayList<String>();
+	private DatabaseHelper db;
+	private Cursor cursor;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +35,51 @@ public class YearActivity extends ListActivity {
 	}
 	
 	private void plusYear() {
-			final View addView=getLayoutInflater().inflate(R.layout.activity_inputyear, null);
+		LayoutInflater inflater = LayoutInflater.from(this);
+		View addView = inflater.inflate(R.layout.activity_inputyear, null);
+		final DialogWrapper wrapper = new DialogWrapper(addView);
 			
 			new AlertDialog.Builder(this)
 				.setTitle("Add a Year")
 				.setView(addView)
 				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int whichButton) {
-						ArrayAdapter<String> listAdapter = (ArrayAdapter<String>)getListAdapter();
-						ArrayAdapter<String> adapter = listAdapter;
-						EditText title = (EditText) addView.findViewById(R.id.et_year);
-						
-						adapter.add(title.getText().toString());
+						processAdd(wrapper);
 					}
 				})
 				.setNegativeButton("Cancel", null)
 				.show();										
+	}
+	
+	private void processAdd(DialogWrapper wrapper) {
+		ContentValues cv = new ContentValues(1);
+		
+		cv.put(DatabaseHelper.YEAR, wrapper.getYear());
+		db.getWritableDatabase().insert("year", DatabaseHelper.YEAR, cv);
+	}
+	
+	private void processDelete(long rowId) {
+		String[] args = {String.valueOf(rowId)};
+		db.getWritableDatabase().delete("year", "_ID=?", args);
+	}
+	
+	class DialogWrapper {
+		EditText year;
+		View base;
+		
+		public DialogWrapper(View base) {
+			this.base = base;
+			year = (EditText) findViewById(R.id.et_year);
+		}
+		
+		String getYear() {
+			return getField().getText().toString(); 
+		}
+		
+		private EditText getField() {
+			if (year == null)
+				year = (EditText) base.findViewById(R.id.et_year);
+			return year;
+		}
 	}
 }
