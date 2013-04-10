7517df456f2453c932a60a60eaf4491175ada1a4
Christopher Hudson <Crh018@bravemail.uncp.edu>
2013-04-09 21:44:19 -0400

    Messing around, Need to merge, have to commit first

--- Course-Organizer/src/com/teamswag/course_organizer/YearActivity.java
+++ Course-Organizer/src/com/teamswag/course_organizer/YearActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -29,7 +30,7 @@ public class YearActivity extends ListActivity {
 		super.onResume();
 	}
 	
-	private void plusYear() {
+	public void plusYear(View v) {
 			final View addView=getLayoutInflater().inflate(R.layout.activity_inputyear, null);
 			
 			new AlertDialog.Builder(this)
