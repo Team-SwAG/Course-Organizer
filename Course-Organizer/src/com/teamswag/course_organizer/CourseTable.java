1e5fc807ce7b442ad54cf2e08449e2df3bdca5a5
Dillon Lees <dillon.lees@gmail.com>
2013-04-09 23:17:03 -0400

    Other than queries, I want to say the database stuff is pretty much done.

--- Course-Organizer/src/com/teamswag/course_organizer/CourseTable.java
+++ Course-Organizer/src/com/teamswag/course_organizer/CourseTable.java
@@ -1,5 +1,28 @@
 package com.teamswag.course_organizer;
 
-public class CourseTable {
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
+public class CourseTable {
+	public static final String NAME = "course";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_YEAR_ID = "year_id";
+	public static final String COLUMN_SEMESTER_ID = "semester_id";
+	
+	public static void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE " + NAME + " ( " + COLUMN_ID
+				+ " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_NAME
+				+ " TEXT NOT NULL, " + COLUMN_YEAR_ID + " INTEGER, "
+				+ COLUMN_SEMESTER_ID + " TEXT NOT NULL);");
+	}
+
+	public static void onUpgrade(SQLiteDatabase db, int oldVersion,
+			int newVersion) {
+
+		Log.w(YearTable.class.getName(), "Upgrading from version " + oldVersion
+				+ " to version " + newVersion + ", which will destroy all data");
+		db.execSQL("DROP TABLE IF EXISTS " + NAME);
+		onCreate(db);
+	}
 }
--- Course-Organizer/src/com/teamswag/course_organizer/SemesterTable.java
+++ Course-Organizer/src/com/teamswag/course_organizer/SemesterTable.java
@@ -1,5 +1,25 @@
 package com.teamswag.course_organizer;
 
-public class SemesterTable {
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
+public class SemesterTable {
+	public static final String NAME = "semester";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_NAME = "name";
+
+	public static void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE " + NAME + " ( " + COLUMN_ID
+				+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
+				+ COLUMN_NAME + " TEXT NOT NULL);");
+	}
+
+	public static void onUpgrade(SQLiteDatabase db, int oldVersion,
+			int newVersion) {
+
+		Log.w(SemesterTable.class.getName(), "Upgrading from version " + oldVersion
+				+ " to version " + newVersion + ", which will destroy all data");
+		db.execSQL("DROP TABLE IF EXISTS " + NAME);
+		onCreate(db);
+	}
 }
