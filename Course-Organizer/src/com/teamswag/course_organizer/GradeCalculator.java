2f128126b0c6b773a8de93db26d771ad2b77e12d
Kevin Ruffin <kevykev1992@yahoo.com>
2013-04-18 21:37:40 -0400

    Where me and gus left off

--- Course-Organizer/src/com/teamswag/course_organizer/GradeCalculator.java
+++ Course-Organizer/src/com/teamswag/course_organizer/GradeCalculator.java
@@ -4,29 +4,45 @@ import java.util.ArrayList;
 
 import android.database.Cursor;
 
-public class GradeCalculator {
+public class GradeCalculators {
 
-	public static String getGrade(String courseId, DatabaseHelper db) {
-		Cursor cursor = db.getReadableDatabase().rawQuery(
-				"SELECT " + CriteriaTable.COLUMN_NAME + ", " + CriteriaTable.COLUMN_WEIGHT + " FROM "
-						+ CriteriaTable.NAME + " WHERE "
-						+ CriteriaTable.COLUMN_COURSE_ID + "=\'" + courseId
-						+ "\'", null);
-		ArrayList<Double> weightList = new ArrayList<Double>();
-		cursor.moveToFirst();
-		while (!cursor.isAfterLast())
-			weightList.add(Double.parseDouble(cursor.getString(0)));
+	
+	
+	String[] criteria = {"Test", "Homework", "Quizzes"};
+	double[] testGrades = {90, 79, 97, 88};
+	double[] homeworkGrades = {83, 55, 46, 78};
+	double[] quizGrades = {97, 67, 77, 87};
+	double testWeight = 0.50;
+	double homeworkWeight = 0.15;
+	double quizWeight = 0.35;
 
-		// Add weightedAverage, weight variable
-		String gradeAverage;
-		double average = 0;
-
-		for (int i = 0; i <= weightList.size(); i++)
-			average += weightList.get(i);
-			average = average / weightList.size();
-			weightedAverage = average * weight;
-			gradeAverage = Double.toString(weightedAverage);
-		return "";
-
-	}
+	GCO = new GCO;
+	
+	
+	for(int i = 0; i < criteria.length(); i++){
+		String x;
+		x = criteria[i];
+	}	
 }
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+    	int avg = 45;
+    	String grade;
+    	switch (avg) {
+    		case (avg>0) && (avg<=60): grade = "F";
+    				break;
+    		case (avg>60) && (avg<=70): grade = "D" ;
+    				break;
+    		case (avg>70) && (avg<=80): grade = "C";
+    				break;
+    		case (avg>80) && (avg<=90): grade = "B";
+    				break;
+    		case (avg>90) && (avg<=100): grade = "A";
+    				break;	
+    	}	
+    }
+    }
+
+
+
